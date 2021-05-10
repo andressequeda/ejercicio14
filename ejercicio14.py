71e1e0b86b61c0e1387f16f80fa342edfd70e17d
Andres Sequeda Higuita <80718240+andressequeda@users.noreply.github.com>
2021-05-09 22:15:43 -0500

    Add files via upload

---
+++ ejercicio14.py
@@ -0,0 +1,3 @@
+masa  =  float ( input ( "Ingrese la masa del objeto (en kilogramos):" ))
+velocidad_de_la_luz  =  299792458
+print ( "La energía del objeto es de" , masa  *  velocidad_de_la_luz  **  2 , "Julios" )
